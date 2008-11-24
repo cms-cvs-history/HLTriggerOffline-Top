22beccca0fa0ddb431a3fe262b5a79bc4f217f5b
cmsbuild <>
2008-11-24 15:07:49 +0000

    This commit was manufactured by cvs2git to create branch 'HLTriggerOffline- Top-branch_finalHepMCRemovalCleanup'.
    Sprout from master 2008-11-24 15:07:47 UTC Patricia Lobelle Pardo <sha1-3dd526f680a6b9402d4d1ca132358364c3ecf037@cern.ch> '*** empty log message ***'
    Delete:
        BuildFile
        doc/html/index.html
        doc/html/overview.html
        python/__init__.pyc
        python/topvalidation_cfi.py
        python/topvalidation_cfi.pyc
        src/TopValidation.cc
        test_cfg.py

--- python/topvalidation_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-HLTTopVal = cms.EDAnalyzer("TopValidation",
-    OutputMEsInRootFile = cms.bool(False),
-    TriggerResultsCollection = cms.InputTag("TriggerResults","","HLT"),
-    OutputFileName = cms.string(''),
-    DQMFolder = cms.untracked.string("HLT/Top")
- )
